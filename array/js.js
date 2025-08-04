05ec85a6543ee406edca1240bc80951998cdbc48
Aliaksei Redzko <aleha.redko@gmail.com>
2025-08-04 22:24:43 +0300

    video lesson 21

--- array/js.js
+++ array/js.js
@@ -50,6 +50,38 @@ function makePhrases(param1, param2, param3) {
 console.log(makePhrases(words4, words2, words3));  // ещё один способ вывода
 
 
+//LESSON 21
+
+
+// Bubble solution #0 score: 60
+// Bubble solution #1 score: 50
+// Bubble solution #2 score: 60
+// Bubbles tests: 36
+// Highest bubble score: 69
+// Solutions with highest score: #11, #18
+
+const scores = [60, 50, 60, 58, 54, 54,
+    58, 50, 52, 54, 48, 69,
+    34, 55, 51, 52, 44, 51,
+    69, 64, 66, 55, 52, 61,
+    46, 31, 57, 52, 44, 18,
+    41, 53, 55, 61, 51, 44];
+
+// let i = 0;
+let output
+
+// while (i < scores.length) {
+//     output = "Bubble solution #" + i + "score:" + scores[i];
+//     console.log(output);
+//     i++
+// }
+
+for (let i = 0; i < scores.length; i++) {
+    output = "Bubble solution #" + i + "score:" + scores[i];
+    console.log(output);
+}
+
+console.log("Bubbles tests: " + scores.length);
 
 
 
