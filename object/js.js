4382fcc2925e4f20b1383ba7a5166fcd7abfde58
Aliaksei Redzko <aleha.redko@gmail.com>
2025-08-05 20:17:59 +0300

    video lesson 25

---
+++ object/js.js
@@ -0,0 +1,35 @@
+//lesson 25
+
+//cars:
+// make: "Chevy",
+// model: "Bel Air",
+// year: 1957,
+// color: "red",
+// passengers: 2,
+// convertible: false,
+// mileage: 102
+
+// dog:
+// name: "Fido",
+// weight: 20.2,
+// age: 4,
+// breed: "mixed",
+// activity: "fetch balls"
+
+const car = {
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    mileage: 1021
+}
+
+const dog = {
+    name: "Fido",
+    weight: 20.2,
+    age: 4,
+    breed: "mixed",
+    activity: "fetch balls"
+}
